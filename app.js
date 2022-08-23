4ccb02aff43f8714ae03012a27eaeff2f08cdb3f
JuanCruzEtchart <etchart.juancruz@gmail.com>
2022-08-23 19:11:14 -0300

    Formularios y pantalla de registro y login

--- app.js
+++ app.js
@@ -8,3 +8,21 @@ app.listen(3000, () => console.log("Servidor corriendo en puerto 3000"));
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/home.html"));
 });
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+
+app.post("/login", (req, res) => {
+  console.log("Ingresaste correctamente!");
+  res.redirect("/");
+});
+
+app.post("/register", (req, res) => {
+  console.log("Te registraste correctamente!");
+  res.redirect("/");
+});
