6ecc0d3a221df708fb4b237526e8cd3ae38804a2
JuanCruzEtchart <etchart.juancruz@gmail.com>
2022-08-29 20:38:43 -0300

    Editado para subir a Heroku

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express();
 const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000, () => console.log("Servidor corriendo en puerto 3000"));
+/*app.listen(3000, () => console.log("Servidor corriendo en puerto 3000"));*/
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Servidor corriendo en puerto 3000");
+});
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/home.html"));
@@ -25,4 +29,4 @@ app.post("/login", (req, res) => {
 app.post("/register", (req, res) => {
   console.log("Te registraste correctamente!");
   res.redirect("/");
-});
+})
